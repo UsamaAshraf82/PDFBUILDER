d2c17ce7220c474e84a027002ca64261788d7eaf
Muhammad Usama Ashraf <32302712+UsamaAshraf82@users.noreply.github.com>
2020-05-05 20:40:57 +0500

    PDFmake

--- src/JSBUILDER/PDFMAKE.js
+++ src/JSBUILDER/PDFMAKE.js
@@ -1,4 +1,3 @@
-// import React from 'react'
 import pdfMake from 'pdfmake'
 
 async function PDFmakeComponent(noOfImage) {
@@ -9,49 +8,43 @@ async function PDFmakeComponent(noOfImage) {
   }
 
   const content = []
+  let width = 0
+  let columns = []
 
-  for (let j = 0; j < noOfImage; j = j + 5) {
+  for (let j = 0; j < noOfImage; j++) {
     console.log(j)
 
-    const columns = []
     const base = await imageToBase64('https://source.unsplash.com/random/')
 
-    console.log(base)
+    if (width > 540) {
+      console.log(columns)
+      content.push({ columns: columns })
+      columns = []
+      width = 0
+    }
+
+    width = width + base.width
 
     columns.push({
-      width: 'auto',
       image: base.base64,
-      fit: [105, 105]
+      width: base.width
     })
-
-    content.push({ columns: columns })
   }
 
+  console.log(content)
   docDefinition.content = content
-  pdfMake.createPdf(docDefinition).download()
+  pdfMake.createPdf(docDefinition).download('A', () => {
+    console.log('done')
+  })
 }
 
 async function imageToBase64(url) {
   let image
   image = new Image()
   image.crossOrigin = 'Anonymous'
-  // image.height = 90
   image.src = url
 
   const imgPro = await a(image)
-
-  // image.addEventListener('load', function () {
-  //   let canvas = document.createElement('canvas')
-  //   let context = canvas.getContext('2d')
-  //   canvas.width = image.width
-  //   canvas.height = image.height
-  //   context.drawImage(image, 0, 0)
-  //   try {
-  //     return canvas.toDataURL('image/png'))
-  //   } catch (err) {
-  //     console.error(err)
-  //   }
-  // })
 
   return new Promise(resolve => {
     resolve(imgPro)
@@ -64,79 +57,16 @@ const a = image =>
       let canvas = document.createElement('canvas')
       let context = canvas.getContext('2d')
 
-      // const height = 90
-      // const x = image.height / height
-      // const width = image.width / x
+      const height = 150
+      const x = image.height / height
+      const width = image.width / x
 
       canvas.width = image.width
       canvas.height = image.height
       context.drawImage(image, 0, 0)
 
-      resolve({ base64: canvas.toDataURL('image/png'), width: image.width })
+      resolve({ base64: canvas.toDataURL('image/png'), width: width })
     }
   })
-
-//   var docwidth = doc.internal.pageSize.getWidth()
-//   var docheight = doc.internal.pageSize.getHeight()
-
-//   const height = 90
-
-//   let previosWidth = 0
-//   let row = 0
-//   for (let j = 0; j < noOfImage; j++) {
-//     const Image = await imgData('https://source.unsplash.com/random/')
-//     const x = Image.height / height
-//     const width = Image.width / x
-
-//     console.log(j)
-
-//     let nextWidth = previosWidth + width
-
-//     if (nextWidth > docwidth) {
-//       row = row + 1
-//       previosWidth = 0
-//       nextWidth = 0
-//     }
-
-//     if (height * row > docheight) {
-//       console.log('here')
-//       doc.addPage()
-//       row = 0
-//     }
-//     doc.addImage(
-//       Image.img,
-//       'png',
-//       previosWidth,
-//       height * row,
-//       width,
-//       height,
-//       undefined,
-//       'FAST'
-//     )
-
-//     previosWidth = width + previosWidth
-//   }
-
-//   doc.save('save.pdf')
-// }
-
-// async function imgData(imgData) {
-//   var img = new Image()
-
-//   img.style.objectFit = 'cover'
-//   img.src = imgData
-//   const imgPro = await a(img)
-
-//   return new Promise(resolve => {
-//     resolve({ img, width: imgPro[0], height: imgPro[1] })
-//   })
-// }
-
-// const a = img =>
-//   new Promise(resolve => {
-//     img.onload = () => {
-//       resolve([img.width, img.height])
-//     }
-//   })
 
 export default PDFmakeComponent
